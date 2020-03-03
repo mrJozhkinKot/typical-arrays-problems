1f3dfd280ab686c30251be5943fc0661f044e465
Yulia Shchuklina <scrambler88@mail.ru>
2020-03-03 22:55:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0;}
+  else {
+  return array.reduce((a,b) => {return (a<b? a: b)});
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0;}
+  else {
+  return array.reduce((a,b) => {return (a>b? a: b)});
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0;}
+  else {
+  let sum = array.reduce((a,b) => {return (a+b)});
+  return sum / array.length;
+  }
 }
